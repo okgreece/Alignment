95de4e357f6e94671988b74a05aace19bf45090c
skarampatakis <s.karampatakis@gmail.com>
2017-01-16 17:22:33 +0200

    Tooltips for Link chooser component using description from according ontology. Working with RDFS, OWL and SKOS.

--- resources/views/createlinks/partials/linkinput.blade.php
+++ resources/views/createlinks/partials/linkinput.blade.php
@@ -1,3 +1,17 @@
 @foreach($instances as $instance)
-<input type="radio" name="link_type" value="{{$instance->value}}" /><i data-toggle="tooltip" data-placement="auto" data-container="body" data-animations="true" title="This option will create a link of type {{ EasyRdf_Namespace::shorten($instance->value)}}" class="fa fa-fw fa-info-circle info-icon"></i> {{$instance->inner}} <br/>
+<?php
+    if(!Illuminate\Support\Facades\Cache::has($instance->group. '.ontology')){
+        $graph = new EasyRdf_Graph;
+        $graph->parseFile(storage_path('app/ontologies/' . strtolower($instance->group) . '.rdf'));
+        Illuminate\Support\Facades\Cache::forever($instance->group. '.ontology' , $graph);
+    }
+    else{
+        $graph = Illuminate\Support\Facades\Cache::get($instance->group. '.ontology');
+    }
+    $description = $graph->get($instance->value, new \EasyRdf_Resource('rdfs:comment'))?:$graph->get($instance->value, new \EasyRdf_Resource('skos:definition'));
+?>
+
+<input type="radio" name="link_type" value="{{$instance->value}}" />
+    <i data-toggle="tooltip" data-placement="auto" data-container="body" data-animations="true" title="This option will create a link of type {{ EasyRdf_Namespace::shorten($instance->value)}}. Definition of link type: {{$description}}" class="fa fa-fw fa-info-circle info-icon"></i>
+    {{$instance->inner}} <br/>
 @endforeach
