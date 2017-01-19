92942a4619ebb74d6870e73094dd484f6b4cb175
skarampatakis <s.karampatakis@gmail.com>
2017-01-19 16:18:58 +0200

    Added unique name validation on projects.

--- resources/views/myprojects.blade.php
+++ resources/views/myprojects.blade.php
@@ -17,6 +17,18 @@
         @include('utility.info.failnotification')        
     @endif
     
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <p>
+            Could not create project:
+        </p>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     @include('projects.projecttable')
         
 @endsection
