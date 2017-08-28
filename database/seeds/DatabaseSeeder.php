ac236e8d8bb2e2d5dfffe5141e0684447ef676ab
sotos <s.karampatakis@gmail.com>
2017-08-28 22:06:52 +0300

    created label extractor seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
          $this->call(\ProjectFilePivotTableSeeder::class);
          $this->call(\SettingsSeeder::class);
          $this->call(\LinkTypeSeeder::class);
-        
+         $this->call(\LabelExtractorSeeder::class);
     }
     
 }
