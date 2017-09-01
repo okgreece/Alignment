5dba12da93a3183c0d1b19ab30fe9860a888db6f
skarampatakis <s.karampatakis@gmail.com>
2017-09-01 12:59:12 +0300

    minor fix in dashboard

--- resources/views/layouts/partials/contentheader.blade.php
+++ resources/views/layouts/partials/contentheader.blade.php
@@ -4,8 +4,8 @@
         @yield('contentheader_title', 'Page Header here')
         <small>@yield('contentheader_description')</small>
     </h1>
-<!--    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-    </ol>-->
+    <ol class="breadcrumb">
+        <li><a href="{{route("dashboard")}}">Alignment</a></li>
+        <li class="active">@yield('contentheader_title', 'Page Header here')</li>
+    </ol>
 </section>
