0353877d481ddcbed3e1490ea57fbfd355e3cf0e
skarampatakis <s.karampatakis@gmail.com>
2018-03-29 19:43:04 +0300

    added tooltip mutator

--- app/File.php
+++ app/File.php
@@ -9,12 +9,11 @@ use Codesleeve\Stapler\ORM\EloquentTrait;
 class File extends Model implements StaplerableInterface {
     use EloquentTrait;
 
-    
+    protected $appends = ["tooltip"];
     protected $fillable = ['resource', 'filetype', 'public','user_id'];
 
     public function __construct(array $attributes = array()) {
-        $this->hasAttachedFile('resource', [
-            
+        $this->hasAttachedFile('resource', [            
         ]);
 
         parent::__construct($attributes);
@@ -27,5 +26,10 @@ class File extends Model implements StaplerableInterface {
     public function projects()
     {
         return $this->belongsToMany('App\Project', 'file_project','file_id','project_id');
+    }
+    
+    public function getTooltipAttribute(){
+        $ids = $this->projects->pluck("id")->toArray();
+        return count($ids) > 0 ? "File in use in project(s) " . implode(", ", $ids) . ". Please, remove them first!" : "";
     }
 }
