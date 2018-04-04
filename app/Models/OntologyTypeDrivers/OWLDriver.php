f885ac156f07e66cf9f2015502b1865eb6856e84
skarampatakis <s.karampatakis@gmail.com>
2018-04-04 13:38:03 +0300

    fixed logging errors on failed jobs

--- app/Models/OntologyTypeDrivers/OWLDriver.php
+++ app/Models/OntologyTypeDrivers/OWLDriver.php
@@ -2,35 +2,47 @@
 
 namespace App\Models\OntologyTypeDrivers;
 
-class OWLDriver
+use App\Models\OntologyTypeDriver;
+
+class OWLDriver extends OntologyTypeDriver
 {
 
-    protected $name = "OWL Driver";
+    const id = "OWL";
+            
+    const name = "OWL Driver";
 
-    protected $description = "" ;
+    const description = "" ;
 
-    protected $root = "owl:Ontology";
+    const root = "owl:Ontology";
+    
+    const firstLevelPath = "rdfs:subClassOf";
+    
+    const inverseFirstLevelPath = "^rdfs:subClassOf";
+    
+    const secondLevelPath = "rdfs:subClassOf";
+    
+    const inverseSecondLevelPath = "^rdfs:subClassOf";
 
-    protected $firstLevel = [
-        [
-            "class" => "owl:Class",
-            "restriction" => "rdfs:subClassOf",
-            "firstLevelPath" => "rdfs:subClassOf"
-        ],
-        [
-            "class" => "owl:ObjectProperty",
-            "restriction" => "rdfs:subPropertyOf",
-            "firstLevelPath" => "rdfs:subPropertyOf"
-        ]
-    ];
-
-
-
-    protected $firstLevelPath = null;
-
-    protected $inverseFirstLevelPath = null;
-
-    protected $secondLevelPath = null;
-
-    protected $inverseSecondLevelPath = null;
+//    protected $firstLevel = [
+//        [
+//            "class" => "owl:Class",
+//            "restriction" => "rdfs:subClassOf",
+//            "firstLevelPath" => "rdfs:subClassOf"
+//        ],
+//        [
+//            "class" => "owl:ObjectProperty",
+//            "restriction" => "rdfs:subPropertyOf",
+//            "firstLevelPath" => "rdfs:subPropertyOf"
+//        ]
+//    ];
+//
+//
+//
+//    protected $firstLevelPath = null;
+//
+//    protected $inverseFirstLevelPath = null;
+//
+//    protected $secondLevelPath = null;
+//
+//    protected $inverseSecondLevelPath = null;
 }
--- database/migrations/2018_04_04_090710_create_failed_jobs_table.php
+++ database/migrations/2018_04_04_090710_create_failed_jobs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -12,11 +13,13 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('failed_jobs');
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
+            $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
         });
     }
@@ -28,6 +31,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('failed_jobs');
+        Schema::dropIfExists('failed_jobs');
     }
 }
