8bf5437d6f25239ad34750220a8b7a91aded5f4c
skarampatakis <s.karampatakis@gmail.com>
2018-04-11 14:54:04 +0300

    changed silk configuration, added config file

--- app/Jobs/RunSilk.php
+++ app/Jobs/RunSilk.php
@@ -20,12 +20,14 @@ class RunSilk extends Job implements ShouldQueue
      *
      * @return void
      */
-    protected $project,$user;
+    protected $project,$user, $configFileName;
     
     public function __construct(Project $project, User $user)
     {
         $this->project = $project;
         $this->user = $user->id;
+        $this->configFileName = storage_path() . "/app/projects/project" . $project->id . "/project" . $project->id . "_config.xml";
+        $this->checkExistingFile($project->id);        
     }
 
     /**
@@ -35,42 +37,43 @@ class RunSilk extends Job implements ShouldQueue
      */
     public function handle()
     {        
-        $this->runSiLK($this->project, $this->user);        
+        $this->runSiLK($this->project);        
     }
     
-    public function runSiLK(Project $project, $user_id) {
-        $id = $project->id;
-        $filename = storage_path() . "/app/projects/project" . $id . "/project" . $id . "_config.xml";
+    private function getConfig($filename){
+        $config = array_merge(
+                ["java"],
+                config("alignment.silk.config", []),
+                [
+                    '-jar',
+                    config("alignment.silk.jar"),
+                    '-DconfigFile=' . $filename
+                ]);        
+        return $config;
+    }
+    
+    public function runSiLK(Project $project) {
         \App\Notification::create([
             "message" => 'Started Job...',
-            "user_id" => $user_id,
+            "user_id" => $this->user,
             "project_id" => $project->id,
             "status" => 2,
-        ]);
-        $config = [
-            'java',
-            '-d64',
-            '-Xms2G',
-            '-Xmx4G',
-            '-DconfigFile=' . $filename,
-            '-Dreload=true',
-            '-Dthreads=4',
-            '-jar',
-            app_path() . '/functions/silk/silk.jar'            
-        ];
-        
+        ]);        
+        $config = $this->getConfig($this->configFileName);
         $process = new Process($config);
-        $process->run();
+        $process->run();        
         \App\Notification::create([
             "message" => 'Finished SiLK similarities Calculations...',
-            "user_id" => $user_id,
+            "user_id" => $this->user,
             "project_id" => $project->id,
             "status" => 2,
         ]);
-        
-//        if (\Storage::disk("projects")->exists("/project" . $project->id . "/score_project" . $project->id . ".nt")) {
-//            \Storage::disk("projects")->delete("/project" . $project->id . "/score_project" . $project->id . ".nt");
-//        }        
-        dispatch(new \App\Jobs\ParseScores($project, $user_id));
+        dispatch(new \App\Jobs\ParseScores($project, $this->user));
+    }
+    
+    private function checkExistingFile($id){
+        if (\Storage::disk("projects")->exists("/project" . $id . "/score_project" . $id . ".nt")) {
+            \Storage::disk("projects")->delete("/project" . $id . "/score_project" . $id . ".nt");
+        }        
     }
 }
